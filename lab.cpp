4435c86379e26caa666710c61b7a93b2de5eb480
evgeshkarub <32188386+evgeshkarub@users.noreply.github.com>
2017-09-22 11:48:06 +0300

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -9,8 +9,58 @@ int main()
 
 
     // 2;
-    std::cout << " 1 13 49 " << std::endl;
+    std::cout <<1<<" "<<13<<" "<<49<< std::endl;
     //////////////////////////////////
+
+    // 3;
+    char r;
+    std::cout << "enter the symbol" << std::endl;
+    std::cin >> r;
+    std::cout <<1<<r<<13<<r<<49<< std::endl;
+    ////////////////////////////////////
+
+
+    // 4;
+    int a, b, c;
+    std::cout << "enter a b c" << std::endl;
+    std::cin >> a >> b >> c;
+    std::cout <<a <<" "<<b <<" "<<c<< std::endl;
+    //////////////////////////////////////
+
+    // 5;
+    double f = 0;
+    double x = 0;
+    double y = 0;
+    std::cout << "enter f" << std::endl;
+    std::cin >> f;
+    x=12*f*f + 7*f - 12;
+    std::cout <<f<< std::endl;
+    y=3*x*x*x + 4*x*x - 11*x + 1;
+    std::cout <<y<< std::endl;
+    //////////////////////////////////////
+
+    // 6;
+    double v = 0;
+    double m = 0;
+    double p = 0;
+    std::cout << "enter volume and mass" << std::endl;
+    std::cin >> v >> m;
+    p=m/v;
+    std::cout <<p<< std::endl;
+    ////////////////////////////////////////
+
+    // 7;
+    double q = 0;
+    double w = 0;
+    double t = 0;
+    std::cout << "enter a and b" << std::endl;
+    t=-w/q;
+
+
+
+
+
+
 
     return 0;
 }
