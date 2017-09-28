a10892d71e214c7cf9a50b5640b7502078072fff
evgeshkarub <32188386+evgeshkarub@users.noreply.github.com>
2017-09-28 23:16:29 +0300

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -1,33 +1,25 @@
 #include <iostream>
 #include <math.h>
 
-
-int main()
-{
+int main() {
 // 1;
 std::cout << 3.14 << std::endl;
 /////////////////////////////////
-
-
 // 2;
 std::cout <<1<<" "<<13<<" "<<49<< std::endl;
 //////////////////////////////////
-
 // 3;
 char symbol;
 std::cout << "enter the symbol" << std::endl;
 std::cin >> symbol;
 std::cout <<1<<r<<13<<r<<49<< std::endl;
 ////////////////////////////////////
-
-
 // 4;
 int a, b, c;
 std::cout << "enter a b c" << std::endl;
 std::cin >> a >> b >> c;
 std::cout <<a <<" "<<b <<" "<<c<< std::endl;
 //////////////////////////////////////
-
 // 5;
 double f = 0;
 double x = 0;
@@ -39,7 +31,6 @@ std::cout <<f<< std::endl;
 y=3*x*x*x + 4*x*x - 11*x + 1;
 std::cout <<y<< std::endl;
 //////////////////////////////////////
-
 // 6;
 double volume = 0;
 double weight = 0;
@@ -49,7 +40,6 @@ std::cin >> volume >> weight;
 density=weight/volume;
 std::cout <<density<< std::endl;
 ////////////////////////////////////////
-
 // 7;
 double q = 0;
 double w = 0;
@@ -58,7 +48,6 @@ std::cout << "enter a and b" << std::endl;
 t=-w/q;
 std::cout <<t<< std::endl;
 //////////////////////////////////////
-
 // 8;
 double x1 = 0;
 double x2 = 0;
@@ -69,7 +58,6 @@ std::cout << "enter x1, x2, y1, y2" << std::endl;
 distance=sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
 std::cout <<distance<< std::endl;
 //////////////////////////////////////
-
 // 9;
 double upperbase = 0;
 double bottombase = 0;
@@ -79,7 +67,6 @@ std::cout << "enter upper base, bottom base, and height" << std::endl;
 perimeter=upperbase + bottombase + 2*sqrt((bottombase - upper base)*(bottombase - upperbase) + height*height);
 std::cout <<perimeter<< std::endl;
 //////////////////////////////////////
-
 // 10;
 double biggerradius = 0;
 double smallerradius = 0;
@@ -88,7 +75,6 @@ std::cout << "enter bigger radius and smaller radius" << std::endl;
 area=bigerradius*biggerradius*3,1415926535 - smallerradius*smallerradius*3,1415926535;
 std::cout <<area<< std::endl;
 //////////////////////////////////////
-
 // 11;
 double edge = 0;
 double cubevolume = 0;
@@ -99,7 +85,6 @@ std::cout <<cubevolume<< std::endl;
 cubearea=edge*edge;
 std::cout <<cubearea<< std::endl;
 //////////////////////////////////////
-
 // 12;
 double side = 0;
 double squareperimeter = 0;
@@ -107,7 +92,6 @@ std::cout << "enter side" << std::endl;
 squareperimeter=side + side + side + side;
 std::cout <<squareperimeter<< std::endl;
 //////////////////////////////////////
-
 // 13;
 double radius = 0;
 double diameter = 0;
