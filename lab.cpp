382e36a4fd7859156aa3dfb263057ed3e882273b
evgeshkarub <32188386+evgeshkarub@users.noreply.github.com>
2017-09-28 23:23:29 +0300

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -26,9 +26,9 @@ double x = 0;
 double y = 0;
 std::cout << "enter a" << std::endl;
 std::cin >> f;
-x=12*f*f + 7*f - 12;
+x = 12*f*f + 7*f - 12;
 std::cout << f << std::endl;
-y=3*x*x*x + 4*x*x - 11*x + 1;
+y = 3*x*x*x + 4*x*x - 11*x + 1;
 std::cout << y << std::endl;
 //////////////////////////////////////
 // 6;
@@ -37,7 +37,7 @@ double weight = 0;
 double density = 0;
 std::cout << "enter volume and weight" << std::endl;
 std::cin >> volume >> weight;
-density=weight/volume;
+density = weight/volume;
 std::cout << density << std::endl;
 ////////////////////////////////////////
 // 7;
@@ -45,7 +45,7 @@ double q = 0;
 double w = 0;
 double t = 0;
 std::cout << "enter a and b" << std::endl;
-t=-w/q;
+t = -w/q;
 std::cout << t << std::endl;
 //////////////////////////////////////
 // 8;
@@ -55,7 +55,7 @@ double y1 = 0;
 double y2 = 0;
 double distance = 0;
 std::cout << "enter x1, x2, y1, y2" << std::endl;
-distance=sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+distance = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
 std::cout << distance << std::endl;
 //////////////////////////////////////
 // 9;
@@ -64,7 +64,7 @@ double bbase = 0;
 double Height = 0;
 double perimeter = 0;
 std::cout << "enter upper base, bottom base, and height" << std::endl;
-perimeter=upbase + bbase + 2*sqrt((bbase - upper base)*(bbase - upbase) + height*height);
+perimeter = upbase + bbase + 2*sqrt((bbase - upper base)*(bbase - upbase) + height*height);
 std::cout << perimeter << std::endl;
 //////////////////////////////////////
 // 10;
@@ -72,7 +72,7 @@ double biggerradius = 0;
 double smallerradius = 0;
 double area = 0;
 std::cout << "enter bigger radius and smaller radius" << std::endl;
-area=bigerradius*biggerradius*3,1415926535 - smallerradius*smallerradius*3,1415926535;
+area = bigerradius*biggerradius*3.1415926535 - smallerradius*smallerradius*3.1415926535;
 std::cout << area << std::endl;
 //////////////////////////////////////
 // 11;
@@ -80,9 +80,9 @@ double edge = 0;
 double cubevolume = 0;
 double cubearea = 0;
 std::cout << "enter edge" << std::endl;
-cubevolume=edge*edge*edge;
+cubevolume = edge*edge*edge;
 std::cout << cubevolume << std::endl;
-cubearea=edge*edge;
+cubearea = edge*edge;
 std::cout << cubearea << std::endl;
 //////////////////////////////////////
 // 12;
